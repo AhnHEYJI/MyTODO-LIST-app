7ec743bfd54c080447e1bc2472ca0c5b24f0574a
AhnHEYJI <dkwlgpwl11@naver.com>
2024-10-28 15:52:53 +0900

    db상태변경및삭제시db삭제구현

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import "./App.css";
 import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  setDoc,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
 
 // DB 파이어베이스설정//
 // Import the functions you need from the SDKs you need
@@ -30,82 +37,117 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
- const TodoItemInputField = (props) => {
-   const [input, setInput] = useState("");
- 
-   const onSubmit = () => {
-     props.onSubmit(input);
-     setInput("");
-   };
- 
-   return (<div>
-     <TextField
-       id="todo-item-input"
-       label="Todo Item"
-       variant="outlined"
-       onChange={(e) => setInput(e.target.value)} value={input}
-     />
-     <Button variant="outlined" onClick={onSubmit}>Submit</Button>
-   </div>);
- };
- 
- const TodoItem = (props) => {
-   const style = props.todoItem.isFinished ? { textDecoration: 'line-through' } : {};
-   return (<li>
-     <span style={style} onClick={() => props.onTodoItemClick(props.todoItem)}>{props.todoItem.todoItemContent}</span>
-     <Button variant="outlined" onClick={() => props.onRemoveClick(props.todoItem)}>Remove</Button>
-   </li>);
- };
- 
- 
- const TodoItemList = (props) => {
-   const todoList = props.todoItemList.map((todoItem, index) => {
-     return <TodoItem
-       key={index}
-       todoItem={todoItem}
-       onTodoItemClick={props.onTodoItemClick}
-       onRemoveClick={props.onRemoveClick}
-     />;
-   });
-   return (<div>
-     <ul>{todoList}</ul>
-   </div>);
- };
- function App() {
+const TodoItemInputField = (props) => {
+  const [input, setInput] = useState("");
+
+  const onSubmit = () => {
+    props.onSubmit(input);
+    setInput("");
+  };
+
+  return (
+    <div>
+      <TextField
+        id="todo-item-input"
+        label="Todo Item"
+        variant="outlined"
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
+      />
+      <Button variant="outlined" onClick={onSubmit}>
+        Submit
+      </Button>
+    </div>
+  );
+};
+
+const TodoItem = (props) => {
+  const style = props.todoItem.isFinished
+    ? { textDecoration: "line-through" }
+    : {};
+  return (
+    <li>
+      <span style={style} onClick={() => props.onTodoItemClick(props.todoItem)}>
+        {props.todoItem.todoItemContent}
+      </span>
+      <Button
+        variant="outlined"
+        onClick={() => props.onRemoveClick(props.todoItem)}
+      >
+        Remove
+      </Button>
+    </li>
+  );
+};
+
+const TodoItemList = (props) => {
+  const todoList = props.todoItemList.map((todoItem, index) => {
+    return (
+      <TodoItem
+        key={index}
+        todoItem={todoItem}
+        onTodoItemClick={props.onTodoItemClick}
+        onRemoveClick={props.onRemoveClick}
+      />
+    );
+  });
+  return (
+    <div>
+      <ul>{todoList}</ul>
+    </div>
+  );
+};
+function App() {
   const [todoItemList, setTodoItemList] = useState([]);
 
-
- const onSubmit = async (newTodoItem) => {
-   const docRef = await addDoc(collection(db, "todoItem"),
-    { todoItemContent: newTodoItem,
-      isFinished: false,
-   });
-    setTodoItemList([...todoItemList, {
-
-      id: docRef.id,
+  const onSubmit = async (newTodoItem) => {
+    const docRef = await addDoc(collection(db, "todoItem"), {
       todoItemContent: newTodoItem,
       isFinished: false,
-    }]);
+    });
+    setTodoItemList([
+      ...todoItemList,
+      {
+        id: docRef.id,
+        todoItemContent: newTodoItem,
+        isFinished: false,
+      },
+    ]);
   };
 
-  const onTodoItemClick = (clickedTodoItem) => {
-    setTodoItemList(todoItemList.map((todoItem) => {
-      if (clickedTodoItem.id === todoItem.id) {
-        return {
-          id: clickedTodoItem.id,
-          todoItemContent: clickedTodoItem.todoItemContent,
-          isFinished: !clickedTodoItem.isFinished,
-        };
-      } else {
-        return todoItem;
-      }
-    }));
+  //클릭했을때 db상태변경
+  const onTodoItemClick = async (clickedTodoItem) => {
+    const todoItemRef = doc(db, "todoItem", clickedTodoItem.id);
+    await setDoc(
+      todoItemRef,
+      { isFinished: !clickedTodoItem.isFinished },
+      { merge: true }
+    );
+
+    setTodoItemList(
+      todoItemList.map((todoItem) => {
+        if (clickedTodoItem.id === todoItem.id) {
+          return {
+            id: clickedTodoItem.id,
+            todoItemContent: clickedTodoItem.todoItemContent,
+            isFinished: !clickedTodoItem.isFinished,
+          };
+        } else {
+          return todoItem;
+        }
+      })
+    );
   };
 
-  const onRemoveClick = (removedTodoItem) => {
-    setTodoItemList(todoItemList.filter((todoItem) => {
-      return todoItem.id !== removedTodoItem.id;
-    }));
+  const onRemoveClick = async (removedTodoItem) => {
+    const todoItemRef = doc(db, "todoItem", removedTodoItem.id); //버튼을 이용해 지울때 DB 환경도 같이 삭제되는 로직입니다./
+    await deleteDoc(todoItemRef);
+
+    setTodoItemList(
+      todoItemList.filter((todoItem) => {
+        return todoItem.id !== removedTodoItem.id;
+      })
+    );
   };
 
   return (
